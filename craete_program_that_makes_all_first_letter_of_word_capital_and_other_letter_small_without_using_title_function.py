7e4900d6ca8ac57fa0ac9b333709a91453538bc5
Batsatsugo <corajianchristian@gmail.com>
2025-03-29 00:21:27 +0800

    pseudocode added

---
+++ craete_program_that_makes_all_first_letter_of_word_capital_and_other_letter_small_without_using_title_function.py
@@ -0,0 +1,9 @@
+# DECLARE text, words, capitalized_word, capitalized_words
+# text = input("Enter a string: ")
+# words = text.split()
+# capitalized_words = []
+# FOR word in words:
+#     capitalized_word = word[0].upper() + word[1:].lower() if word else ""
+#     capitalized_words.append(capitalized_word)
+# result = " ".join(capitalized_words)
+# PRINT("Formatted String:", result)
