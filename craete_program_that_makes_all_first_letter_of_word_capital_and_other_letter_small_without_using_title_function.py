cff2e61f8b4864eb1d2d6edf8ecf46a8ad983dbf
Batsatsugo <corajianchristian@gmail.com>
2025-03-29 00:23:13 +0800

    code finished

--- craete_program_that_makes_all_first_letter_of_word_capital_and_other_letter_small_without_using_title_function.py
+++ craete_program_that_makes_all_first_letter_of_word_capital_and_other_letter_small_without_using_title_function.py
@@ -1,9 +1,21 @@
 # DECLARE text, words, capitalized_word, capitalized_words
+
 # text = input("Enter a string: ")
+text = input("Enter a string: ")
+
 # words = text.split()
+words = text.split()
+
 # capitalized_words = []
 # FOR word in words:
 #     capitalized_word = word[0].upper() + word[1:].lower() if word else ""
 #     capitalized_words.append(capitalized_word)
+capitalized_words = []
+for word in words:
+     capitalized_word = word[0].upper() + word[1:].lower() if word else ""
+     capitalized_words.append(capitalized_word)
+
 # result = " ".join(capitalized_words)
 # PRINT("Formatted String:", result)
+result = " ".join(capitalized_words)
+print("Formatted String:", result)
