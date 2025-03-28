4aa6c757718b6cc4ca55c1a424e2b0b39250c0a2
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 23:21:24 +0800

    pseudocode added

---
+++ create_program_that_check_if_string_end_part_matches_the_parameter_without_using_endswith_fucntion.py
@@ -0,0 +1,7 @@
+# DECLARE word, suffix
+# word = input("Enter a word: ")
+# suffix = input("Enter the suffix to check if it matches the end part: ")
+# IF string [-len(suffix):] == suffix:
+#     print("True")
+# ELSE:(
+#     print("False"))
