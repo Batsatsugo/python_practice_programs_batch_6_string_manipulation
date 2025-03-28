0fd8148f17d0c6cad59ecfd4d7f71d19c61f6676
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 22:50:51 +0800

    output

--- program_remove_characters_at_beginning_of_string_that_matches_function_parameter_without_using_remove_prefix_function.py
+++ program_remove_characters_at_beginning_of_string_that_matches_function_parameter_without_using_remove_prefix_function.py
@@ -1,9 +1,21 @@
-# DECLARE word,new_word, prefix,
+# DECLARE word,new_word, prefix
+
 # word = input("Enter a word: ")
-# prefix = input("Enter the prefix to be removed: ")
+word = input("Enter a word: ").strip()
+
+# prefix = input(:Enter the prefix to be removed:")
+prefix = input("Enter the prefix to remove: ").strip()
+
 # IF word[:len(prefix)] == prefix:
 #     new_word = word[len(prefix):]
 # ELSE:
 #     new_word = word
+if len(prefix) <= len(word) and word[:len(prefix)] == prefix:
+    new_word = word[len(prefix):]
+else:
+    new_word = word
+
 # PRINT("Original word:", repr(word))
 # PRINT("Modified word:", repr(new_word))
+print("Original word:", repr(word))
+print("Modified word:", repr(new_word))
