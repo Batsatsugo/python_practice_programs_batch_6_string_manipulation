2e53b107706adcfadee5485094fff080bbd9837a
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 22:34:33 +0800

    pseudocode added

---
+++ program_remove_characters_at_beginning_of_string_that_matches_function_parameter_without_using_remove_prefix_function.py
@@ -0,0 +1,9 @@
+# DECLARE word,new_word, prefix,
+# word = input("Enter a word: ")
+# prefix = input("Enter the prefix to be removed: ")
+# IF word[:len(prefix)] == prefix:
+#     new_word = word[len(prefix):]
+# ELSE:
+#     new_word = word
+# PRINT("Original word:", repr(word))
+# PRINT("Modified word:", repr(new_word))
