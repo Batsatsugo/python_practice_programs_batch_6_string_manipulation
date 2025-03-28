545f5105d0bcc2c1f78dfce7ddc6676e942a0530
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 23:36:25 +0800

    code finished

--- create_program_that_add_space_characters_at_end_to_complete_characters_in_parameter_without_ljust_function.py
+++ create_program_that_add_space_characters_at_end_to_complete_characters_in_parameter_without_ljust_function.py
@@ -1,5 +1,11 @@
 # DECLARE word, width, result
+
 # word = input("Enter a number: ")
 # width = int(input("Enter thw width: "))
 # result = word + " " * (width - len(word))
+word = input("Enter a word: ")
+width = int(input("Enter thw width: "))
+result = word + " " * (width - len(word))
+
 # PRINT(result)
+print(result)
