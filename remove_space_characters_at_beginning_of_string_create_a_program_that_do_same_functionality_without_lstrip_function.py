fe38a20495e25e01e75d3b7c8c11acdd2167efba
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 22:14:12 +0800

    pseudocode added

---
+++ remove_space_characters_at_beginning_of_string_create_a_program_that_do_same_functionality_without_lstrip_function.py
@@ -0,0 +1,8 @@
+# DECLARE word, new_word, space
+# word = "  Jian Cora"
+# space = 0
+# WHILE space < len(word) and word[space] == " ":
+#     space += 1
+# new_word = word[space:]
+# PRINT("Original word:", repr(word))
+# PRINT("Modified word:", repr(new_word))
