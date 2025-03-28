3f0093d561a1e8592ae765584b596f17afb6bac1
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 23:01:21 +0800

    result output

--- create_program_that_converts_all_characters_to_lower_cases_without_using_lower_function.py
+++ create_program_that_converts_all_characters_to_lower_cases_without_using_lower_function.py
@@ -1,9 +1,20 @@
 # DECLARE word, result,
+
 # word = input("Enter a word: ")
 # result = ""
+word = input("Enter a word: ")
+result = ""
+
 # FOR char in word:
 #     IF "A"<= char <= "Z":
 #         result += chr(ord(char) + 32)
 #     ELSE:
 #         result += char
+for char in word:
+    if "A" <= char <= "Z":
+        result += chr(ord(char) + 32)
+    else:
+        result += char
+
 # PRINT("Lowercase word: ", result)
+print("Lowercase word: ", result)
