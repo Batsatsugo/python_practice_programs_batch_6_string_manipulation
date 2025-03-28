b3bde27b4ede0dcd2205dd67f2053bff8f25316b
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 23:23:54 +0800

    result output

--- create_program_that_check_if_string_end_part_matches_the_parameter_without_using_endswith_fucntion.py
+++ create_program_that_check_if_string_end_part_matches_the_parameter_without_using_endswith_fucntion.py
@@ -1,7 +1,15 @@
 # DECLARE word, suffix
+
 # word = input("Enter a word: ")
 # suffix = input("Enter the suffix to check if it matches the end part: ")
-# IF string [-len(suffix):] == suffix:
+word = input("Enter a word: ")
+suffix = input("Enter the suffix to check if it matches the end part: ")
+
+# IF word [-len(suffix):] == suffix:
 #     print("True")
 # ELSE:(
 #     print("False"))
+if word[-len(suffix):] == suffix:
+    print("True")
+else:
+    print("False")
