5b8701a778bab7c3e3d97d748538cf9ba01ea2b1
Batsatsugo <corajianchristian@gmail.com>
2025-03-29 00:09:21 +0800

    pseudocode added

---
+++ craete_program_that_makes_first_letter_of_string_capital_and_other_in_small_cases_without_using_capitalized_functions.py
@@ -0,0 +1,9 @@
+# DECLARE word, first_char, remaining_char, capitalized_word
+# word = input("Enter a word: ")
+# IF len(word) > 0:
+#     first_char = word[0].upper()  # Convert first character to uppercase
+#     remaining_chars = word[1:].lower()  # Convert remaining characters to lowercase
+#     capitalized_word = first_char + remaining_chars
+# ELSE:
+#     capitalized_word = word  # Handle empty word case
+# PRINT("Capitalized Word:", capitalized_word)
