d77689eccf22a4430a4c5019cb102f4b27d59da8
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 23:42:34 +0800

    pseudocode added

---
+++ create_program_that_add_space_characters_at_beginning_and_end_of_string_without_center_funtion.py
@@ -0,0 +1,11 @@
+# DECLARE word, width, total_spaces, left_spaces, right_spaces, centered_word
+# word = input("Enter the word: ")
+# width = int(input("Enter the total width: "))
+# IF width <= len(word):
+#     PRINT(word)
+# ELSE:
+#     total_spaces = width - len(word)
+#     left_spaces = total_spaces // 2
+#     right_spaces = total_spaces - left_spaces
+# centered_word = " " * left_spaces + word + " " * right_spaces
+#     PRINT(centered_word)
