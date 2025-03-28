9adeb144b6e3d8d9897dd478c6104b7d41655f29
Batsatsugo <corajianchristian@gmail.com>
2025-03-28 22:22:53 +0800

    print output

--- remove_space_characters_at_beginning_of_string_create_a_program_that_do_same_functionality_without_lstrip_function.py
+++ remove_space_characters_at_beginning_of_string_create_a_program_that_do_same_functionality_without_lstrip_function.py
@@ -1,8 +1,21 @@
 # DECLARE word, new_word, space
+
 # word = "  Jian Cora"
+word = input("Enter a word with spaces in front before the word: ")
+
 # space = 0
+
+space = 0
 # WHILE space < len(word) and word[space] == " ":
 #     space += 1
+
+while space < len(word) and word[space] == " ":
+    space += 1
+
 # new_word = word[space:]
+new_word =  word[space:]
+
 # PRINT("Original word:", repr(word))
 # PRINT("Modified word:", repr(new_word))
+print("Original word:", repr(word))
+print("Modified word:", repr(new_word))
